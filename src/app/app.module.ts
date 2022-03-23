6a2fab8ea0f473a63fafe8927c4f8e69d25cf83e
Ryan Harrison <mail@ryanharrison.co.uk>
2022-03-23 22:53:52 +0000

    Add loading bar for http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {AppComponent} from './app.component';
 import {SharedModule} from "@shared/shared.module";
 import {HIGHLIGHT_OPTIONS} from "ngx-highlightjs";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {LoadingBarHttpClientModule} from "@ngx-loading-bar/http-client";
 
 function getHighlightLanguages() {
   return {
@@ -42,6 +43,7 @@ function getHighlightLanguages() {
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    LoadingBarHttpClientModule,
     SharedModule,
   ],
   providers: [{

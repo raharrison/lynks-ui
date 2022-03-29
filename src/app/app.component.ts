7958a335f280c129d63bf372b5ef97f319f354a6
Ryan Harrison <mail@ryanharrison.co.uk>
2022-03-29 22:49:31 +0100

    Don't update title when only url fragment is changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import {filter} from 'rxjs/operators';
 export class AppComponent {
   title = 'Lynks';
 
-  constructor(public router: Router, private titleService: Title) {
+  constructor(private router: Router, private titleService: Title) {
     this.router.events
       .pipe(filter(event => event instanceof ChildActivationEnd))
       .subscribe(event => {
@@ -19,12 +19,15 @@ export class AppComponent {
         while (snapshot.firstChild !== null) {
           snapshot = snapshot.firstChild;
         }
-        // take new title from route data if available, otherwise use default
-        let newTitle = this.title;
-        if (snapshot && snapshot.data.title) {
-          newTitle = `${snapshot.data.title} - ${this.title}`;
+        // if fragment has changed keep the current title
+        if (!snapshot.fragment) {
+          // take new title from route data if available, otherwise use default
+          let newTitle = this.title;
+          if (snapshot && snapshot.data.title) {
+            newTitle = `${snapshot.data.title} - ${this.title}`;
+          }
+          this.titleService.setTitle(newTitle);
         }
-        this.titleService.setTitle(newTitle);
       });
   }
 }

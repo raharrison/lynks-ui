01dcc5b342510caf4b83440e35ce6d2e75169142
Ryan Harrison <mail@ryanharrison.co.uk>
2022-04-07 19:26:11 +0100

    Background styling

--- src/app/dashboard/components/index.ts
+++ src/app/dashboard/components/index.ts
@@ -1,10 +1,2 @@
-import {DashboardCardComponent} from './dashboard-card/dashboard-card.component';
-import {DashboardHeadComponent} from "./dashboard-head/dashboard-head.component";
 
-export const components = [
-    DashboardCardComponent,
-    DashboardHeadComponent
-];
-
-export * from './dashboard-card/dashboard-card.component';
-export * from './dashboard-head/dashboard-head.component'
+export const components = [];

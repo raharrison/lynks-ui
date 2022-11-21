c94bcefade008dd468b8ae00017fcac8b86d775c
Ryan Harrison <mail@ryanharrison.co.uk>
2022-11-21 19:56:35 +0000

    Styling for group lists

--- src/app/navigation/containers/collection-list/collection-list.component.ts
+++ src/app/navigation/containers/collection-list/collection-list.component.ts
@@ -1,8 +1,11 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Router} from "@angular/router";
 import {Collection} from "@shared/models";
 import {Subscription} from "rxjs";
 import {CollectionService} from "@shared/services/collection.service";
 import {RouteProviderService} from "@shared/services/route-provider.service";
+import {EntryFilterService} from "@shared/services/entry-filter.service";
+import {TreeComponent} from "@bugsplat/angular-tree-component";
 
 @Component({
   selector: 'lks-collection-list',
@@ -12,27 +15,64 @@ import {RouteProviderService} from "@shared/services/route-provider.service";
 })
 export class CollectionListComponent implements OnInit, OnDestroy {
 
+  @ViewChild('tree')
+  treeComponent: TreeComponent;
+
   collections: Collection[];
   expanded: boolean = false;
 
+  private collectionFilterChanges = [];
   private collectionSubscription: Subscription;
+  private entryFilterSubscription: Subscription;
 
-  constructor(private collectionService: CollectionService,
-              private changeDetectorRef: ChangeDetectorRef,
-              public routeProvider: RouteProviderService) {
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+              private router: Router,
+              private collectionService: CollectionService,
+              private entryFilterService: EntryFilterService,
+              private routeProvider: RouteProviderService) {
   }
 
   ngOnInit(): void {
-    this.collectionSubscription = this.collectionService.$collectionsFlattened.subscribe(value => {
+    this.collectionSubscription = this.collectionService.$collections.subscribe(value => {
         this.collections = value;
         this.changeDetectorRef.markForCheck();
       }
     );
   }
 
+  onTreeLoaded() {
+    if (this.entryFilterSubscription != null) {
+      return;
+    }
+    this.entryFilterSubscription = this.entryFilterService.$entryFilter.subscribe(filter => {
+      if (filter.collections.length === 0) {
+        this.treeComponent.treeModel.setActiveNode(null, false);
+        this.treeComponent.treeModel.setFocusedNode(null);
+      } else {
+        this.collectionFilterChanges = filter.collections;
+        const nodes = filter.collections.map(t => this.treeComponent.treeModel.getNodeById(t));
+        this.treeComponent.treeModel.setActiveNode(null, false);
+        this.treeComponent.treeModel.setFocusedNode(null);
+        nodes.forEach(node => this.treeComponent.treeModel.setActiveNode(node, true, true));
+      }
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
+  onCollectionSelected(collection) {
+    // only change route if collection selected by user, not background filter change
+    if (this.collectionFilterChanges.length === 0) {
+      this.router.navigate([this.routeProvider.baseEntryPath], {queryParams: {collections: collection.id}});
+    }
+    this.collectionFilterChanges = this.collectionFilterChanges.filter(t => t !== collection.id);
+  }
+
   ngOnDestroy(): void {
     if (this.collectionSubscription != null) {
       this.collectionSubscription.unsubscribe();
+    }
+    if (this.entryFilterSubscription != null) {
+      this.entryFilterSubscription.unsubscribe();
     }
   }
 
--- src/app/navigation/containers/tag-list/tag-list.component.ts
+++ src/app/navigation/containers/tag-list/tag-list.component.ts
@@ -1,8 +1,11 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Router} from "@angular/router";
 import {TagService} from "@shared/services/tag.service";
 import {Tag} from "@shared/models";
 import {Subscription} from "rxjs";
 import {RouteProviderService} from "@shared/services/route-provider.service";
+import {EntryFilterService} from "@shared/services/entry-filter.service";
+import {TreeComponent} from "@bugsplat/angular-tree-component";
 
 @Component({
   selector: 'lks-tag-list',
@@ -12,14 +15,21 @@ import {RouteProviderService} from "@shared/services/route-provider.service";
 })
 export class TagListComponent implements OnInit, OnDestroy {
 
+  @ViewChild('tree')
+  treeComponent: TreeComponent;
+
   tags: Tag[];
   expanded: boolean = false;
 
+  private tagFilterChanges = [];
   private tagSubscription: Subscription;
+  private entryFilterSubscription: Subscription;
 
-  constructor(private tagService: TagService,
-              private changeDetectorRef: ChangeDetectorRef,
-              public routeProvider: RouteProviderService) {
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+              private router: Router,
+              private tagService: TagService,
+              private entryFilterService: EntryFilterService,
+              private routeProvider: RouteProviderService) {
   }
 
   ngOnInit(): void {
@@ -30,9 +40,39 @@ export class TagListComponent implements OnInit, OnDestroy {
     );
   }
 
+  onTreeLoaded() {
+    if (this.entryFilterSubscription != null) {
+      return;
+    }
+    this.entryFilterSubscription = this.entryFilterService.$entryFilter.subscribe(filter => {
+      if (filter.tags.length === 0) {
+        this.treeComponent.treeModel.setActiveNode(null, false);
+        this.treeComponent.treeModel.setFocusedNode(null);
+      } else {
+        this.tagFilterChanges = filter.tags;
+        const nodes = filter.tags.map(t => this.treeComponent.treeModel.getNodeById(t));
+        this.treeComponent.treeModel.setActiveNode(null, false);
+        this.treeComponent.treeModel.setFocusedNode(null);
+        nodes.forEach(node => this.treeComponent.treeModel.setActiveNode(node, true, true));
+      }
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
+  onTagSelected(tag) {
+    // only change route if tag selected by user, not background filter change
+    if (this.tagFilterChanges.length === 0) {
+      this.router.navigate([this.routeProvider.baseEntryPath], {queryParams: {tags: tag.id}});
+    }
+    this.tagFilterChanges = this.tagFilterChanges.filter(t => t !== tag.id);
+  }
+
   ngOnDestroy(): void {
     if (this.tagSubscription != null) {
       this.tagSubscription.unsubscribe();
+    }
+    if (this.entryFilterSubscription != null) {
+      this.entryFilterSubscription.unsubscribe();
     }
   }
 
